d81ae50d5d16b50629f7b6790df357192b60f433
Scherazade Odoi <scherazadeodoi366@gmail.com>
2024-10-31 11:23:16 +0000

    router in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 
 function App() {
+  const router = createBrowserRouter([
+{
   
+}
+
+  ])
 
   return (
-    <div></div>
+    <div>
+      <RouterProvider router ={router}/>
+    </div>
   )
 }
 
