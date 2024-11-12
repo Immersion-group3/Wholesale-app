9815acf600e9af981135cd46dd5ad43e00c4ac87
Benedicta Davour <benedictadavour777@gmail.com>
2024-11-12 08:46:25 +0000

    Integrated for order tracking and dashboard.

--- src/services(vendor)/dummyorders.js
+++ src/services(vendor)/dummyorders.js
@@ -1,37 +1,37 @@
-export const dummyOrders = [
-    {
-        order: [
-            {
-                orderId: 97 - 1 - 233 - 4,
-                orderstatus: "Pending",
-                orderplaceddate: "29/09/24",
-                orderpaiddate: "29/09/24",
-            }
-        ],
+// export const dummyOrders = [
+//     {
+//         order: [
+//             {
+//                 orderId: 97 - 1 - 233 - 4,
+//                 orderstatus: "Pending",
+//                 orderplaceddate: "29/09/24",
+//                 orderpaiddate: "29/09/24",
+//             }
+//         ],
         
-        clentdetails:[
-            {
-                name:"Chris Tucker",
-                phone:"8 99 88 77 66",
-                email:"christucker@gmail.com",
-                address:"3633 Douala Cameroun",
-                payment:"Bank card"
-            }
-        ],
-        deliverydetails:[
-            {
-                recipient:"Mary Butcher",
-                address:"3633 Douala Cameroun",
-                special:"Ground Foor B4"
-            }
-        ],
-        orderedItemList:[
-            {
-                productName:"Carrots",
-                qnty:"3",
-                unitPrice:"1500",
-                totalPrice:"4500"
-            }
-        ]
-    }
-]
+//         clentdetails:[
+//             {
+//                 name:"Chris Tucker",
+//                 phone:"8 99 88 77 66",
+//                 email:"christucker@gmail.com",
+//                 address:"3633 Douala Cameroun",
+//                 payment:"Bank card"
+//             }
+//         ],
+//         deliverydetails:[
+//             {
+//                 recipient:"Mary Butcher",
+//                 address:"3633 Douala Cameroun",
+//                 special:"Ground Foor B4"
+//             }
+//         ],
+//         orderedItemList:[
+//             {
+//                 productName:"Carrots",
+//                 qnty:"3",
+//                 unitPrice:"1500",
+//                 totalPrice:"4500"
+//             }
+//         ]
+//     }
+// ]
